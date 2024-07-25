19b8a89dc2b4c6a8c42828fe991452d2a84c12ce
earbuds23 <165831312+earbuds23@users.noreply.github.com>
2024-07-25 20:02:05 +0300

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,27 +1,44 @@
 import React, { useEffect, useState } from 'react';
-import { Link as ScrollLink } from 'react-scroll';
-import { Link as RouterLink, useLocation } from 'react-router-dom';
+import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll';
+import { Link as RouterLink, useLocation, /*useNavigate */ } from 'react-router-dom';
 import './Navbar.css';
 import './index.css';
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
   const location = useLocation();
-  const notHome = location.pathname !== '/'
+  const notHome = location.pathname !== '/';
+//  const [searchQuery, setSearchQuery] = useState('');
+//  const navigate = useNavigate();
 
   useEffect(() => {
     const handleScroll = () => {
       window.scrollY > 40 ? setSticky(true) : setSticky(false);
     };
-    
+
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
-  
+
   const [mobileMenu, setMobileMenu] = useState(false);
   const toggleMenu = () => {
     setMobileMenu(!mobileMenu);
   };
+
+  const handleHomeClick = () => {
+    if (notHome) {
+      window.location.href = '/';
+    } else {
+      scroll.scrollToTop();
+    }
+  };
+
+//  const handleSearch = (e) => {
+  //  e.preventDefault();
+  //  if (searchQuery.trim()) {
+  //    navigate(`/search?query=${searchQuery}`);
+  //  }
+//  };
 
   return (
     <nav className={`container ${sticky || notHome ? 'dark-nav' : ''}`}>
@@ -30,19 +47,45 @@ const Navbar = () => {
       </RouterLink>
       <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
         <li>
-          <ScrollLink to="/home" smooth={true} offset={0} duration={500}>Home</ScrollLink>
+          <RouterLink to="/" onClick={handleHomeClick}>
+            Home
+          </RouterLink>
         </li>
         <li>
-          <ScrollLink to="services" smooth={true} offset={-260} duration={500}>Services</ScrollLink>
+          {notHome ? (
+            <RouterLink to="/#services">Services</RouterLink>
+          ) : (
+            <ScrollLink to="services" smooth={true} offset={-260} duration={500}>
+              Services
+            </ScrollLink>
+          )}
         </li>
         <li>
-          <ScrollLink to="about" smooth={true} offset={-260} duration={500}>About</ScrollLink>
+          {notHome ? (
+            <RouterLink to="/#about">About</RouterLink>
+          ) : (
+            <ScrollLink to="about" smooth={true} offset={-260} duration={500}>
+              About
+            </ScrollLink>
+          )}
         </li>
         <li>
-          <ScrollLink to="testimonials" smooth={true} offset={-250} duration={500}>Testimonials</ScrollLink>
+          {notHome ? (
+            <RouterLink to="/#testimonials">Testimonials</RouterLink>
+          ) : (
+            <ScrollLink to="testimonials" smooth={true} offset={-250} duration={500}>
+              Testimonials
+            </ScrollLink>
+          )}
         </li>
         <li>
-          <ScrollLink to="contact" smooth={true} offset={-250} duration={500}>Contact</ScrollLink>
+          {notHome ? (
+            <RouterLink to="/#contact">Contact</RouterLink>
+          ) : (
+            <ScrollLink to="contact" smooth={true} offset={-250} duration={500}>
+              Contact
+            </ScrollLink>
+          )}
         </li>
         <li>
           <RouterLink to="/blogPage">Blog</RouterLink>
@@ -50,18 +93,19 @@ const Navbar = () => {
         <li className="dropdown">
           <button className="dropbtn">Sign Up</button>
           <div className="dropdown-content">
-            <RouterLink to="/register/customer">Customer</RouterLink>
+            <RouterLink to="/register/consumer">Consumer</RouterLink>
             <RouterLink to="/register/service-provider">Service Provider</RouterLink>
           </div>
         </li>
         <li className="dropdown">
           <button className="dropbtn">Login</button>
           <div className="dropdown-content">
-            <RouterLink to="/login/customer">Customer</RouterLink>
+            <RouterLink to="/login/consumer">Consumer</RouterLink>
             <RouterLink to="/login/service-provider">Service Provider</RouterLink>
           </div>
-        </li>
 
+  
+        </li>
       </ul>
       <img src="/menu-icon.png" className='menu-icon' onClick={toggleMenu} alt='Menu Icon' />
     </nav>
@@ -69,3 +113,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
